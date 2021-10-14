910f51610ca1055968ef8c345105d7e9d286dbb0
Alexander Matzen <alexander@amatzen.dk>
2021-10-14 15:40:24 +0200

    wip: script.py

--- Lesson7/lab/ex3/script.py
+++ Lesson7/lab/ex3/script.py
@@ -4,7 +4,6 @@ from __future__ import print_function, absolute_import, division
 import logging
 import mysql.connector
 
-from errno import ENOENT
 from stat import S_IFDIR, S_IFREG
 from time import time
 
@@ -20,8 +19,12 @@ class Context(LoggingMixIn, Operations):
         else:
             filename = path[1:]  # Remove leading /
 
-        # TODO: If filename does not exist, raise exception: raise FuseOSError(ENOENT)
-        # If file exists, set: st = dict(st_mode=(S_IFREG | 0o755), st_size=filesize)
+
+            if filename == '' or filename is None:
+                raise FuseOSError("ENOENT")
+
+
+            st = dict(st_mode=(S_IFREG | 0o755), st_size=filesize)
 
         st['st_ctime'] = st['st_mtime'] = st['st_atime'] = time()
         return st
@@ -32,12 +35,20 @@ class Context(LoggingMixIn, Operations):
             return ('%s\n' % x).encode('utf-8')
 
         filename = path[1:]
+        if filename == '' or filename is None:
+            raise FuseOSError("ENOENT")
+
+        c = db.cursor(prepared=True)
+        c.execute("SELECT * FROM Files WHERE filename = %s", (filename))
+
+        return self
 
         # TODO: if filename does not exist, raise: raise FuseOSError(ENOENT)
         # If filename exists, return file content
 
     def readdir(self, path, fh):
         Files = []
+
         # TODO: Files should contain list of filenames in directory.     
         return ['.', '..'] + Files
 
@@ -61,7 +72,22 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # TODO: Establish mysql connection
+    db = mysql.connector.Connect(
+        host="db",
+        user="root",
+        passwd="test",
+        database="test"
+    )
+
+    db.cursor().execute("""
+        CREATE TABLE IF NOT EXISTS Files (
+            fileid INT AUTO_INCREMENT,
+            filename VARCHAR(255),
+            filesize INT,
+            filecontent BLOB       
+        );
+    """)
 
     logging.basicConfig(level=logging.WARNING)
     fuse = FUSE(
-        Context(), args.mount, foreground=True, ro=True, allow_other=True)
+        Context(c), args.mount, foreground=True, ro=True, allow_other=True)
