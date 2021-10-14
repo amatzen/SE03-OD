529c4a262af01750405222d27e945487a11f4c1a
Alexander Matzen <alexander@amatzen.dk>
2021-10-14 14:52:03 +0200

    Added start of project incl. docker

--- Lesson7/lab/ex3/script.py
+++ Lesson7/lab/ex3/script.py
@@ -18,18 +18,18 @@ class Context(LoggingMixIn, Operations):
         if path == "/":
             st = dict(st_mode=(S_IFDIR | 0o755), st_nlink=2)
         else:
-            filename = path[1:] # Remove leading /
-        
-         # TODO: If filename does not exist, raise exception: raise FuseOSError(ENOENT)
-         # If file exists, set: st = dict(st_mode=(S_IFREG | 0o755), st_size=filesize)
-         
+            filename = path[1:]  # Remove leading /
+
+        # TODO: If filename does not exist, raise exception: raise FuseOSError(ENOENT)
+        # If file exists, set: st = dict(st_mode=(S_IFREG | 0o755), st_size=filesize)
+
         st['st_ctime'] = st['st_mtime'] = st['st_atime'] = time()
         return st
 
     def read(self, path, size, offset, fh):
 
         def encoded(x):
-            return ('%s\n' % x).encode('utf-8')        
+            return ('%s\n' % x).encode('utf-8')
 
         filename = path[1:]
 
@@ -55,6 +55,7 @@ class Context(LoggingMixIn, Operations):
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('mount')
     args = parser.parse_args()
